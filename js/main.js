8d908b59e3277688f611e4eebf8c5283d7359441
HlinkaYeva <88224819+HlinkaYeva@users.noreply.github.com>
2022-01-05 19:19:21 +0200

    Half of the project
    trying React

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const searchBtn = document.querySelector('#search-btn'),
+      searchForm = document.querySelector('.header .search-form');
+
+searchBtn.onclick = () => {
+    searchBtn.classList.toggle('fa-times');
+    searchForm.classList.toggle('active');
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+const menuBtn = document.querySelector('#menu-btn'),
+      navbar = document.querySelector('.header .navbar');
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    searchBtn.classList.remove('fa-times');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-times');
+    searchForm.classList.remove('active');
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
---
+++ js/react.js
@@ -0,0 +1,81 @@
+const menu = document.getElementById('menu');
+
+function Heading() {
+    return (
+        <div className="heading">
+            <img src="img/title-img.png" alt=""/>
+            <h3>our menu</h3>
+        </div>
+    )
+}
+class HeadingDraw extends React.Component {
+    render() {
+        return (
+            <Heading />
+        )
+    }
+}
+
+
+function Product(props) {
+    return (
+        <div className="box">
+                <img src={props.product.nameImg} alt={props.product.name}/>
+                <div className="content">
+                    <div className="stars">
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star-half-alt"></i>
+                    </div>
+                    <h3>{props.product.name}</h3>
+                    <div className="price">{props.product.price}</div>
+                    <a href="#" className="btn">add to cart</a>
+                </div>
+        </div>
+    )
+}
+
+class OutputToPage extends React.Component {
+    state = {
+        products: [
+            { name: 'cheese hamburger', nameImg: 'img/product-1.png', price: '25.99$' },
+            { name: 'double cheeseburger', nameImg: 'img/product-2.png', price: '24.99$' },
+            { name: 'Big Mac ', nameImg: 'img/product-3.png', price: '20.99$' },
+            { name: 'cheeseburger', nameImg: 'img/product-4.png', price: '26.99$' },
+            { name: 'quarter pounder', nameImg: 'img/product-5.png', price: '15.99$' },
+            { name: 'McDouble', nameImg: 'img/product-6.png', price: '18.99$' }
+        ]
+    }
+    renderProducts() {
+        return this.state.products.map(product => {
+            return (
+                <Product 
+                    product = {product} key = {product.name + Math.random()}
+                />
+            )
+        })
+    }
+    render() {
+        
+        return (
+            <div className="box-container">
+                { this.renderProducts() }
+            </div>
+        )
+    }
+}
+
+class Okey extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <HeadingDraw />
+                <OutputToPage />
+            </React.Fragment>
+        )
+    }
+}
+
+ReactDOM.render(<Okey />, menu);
